0d941c27f372c3808a4dbbcd0e245203af142b1c
chb50 <cedricblake48@gmail.com>
2017-01-31 12:06:26 -0500

    finished level 2, still passes elevel 1 stuff

--- homework1/p05.c
+++ homework1/p05.c
@@ -3,6 +3,12 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <assert.h>
+
+//Unix System Calls
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h> 
 //may need stdlib.h
 
 #define FILE_SIZE 10485764 //10MB + 4 Bytes for padding
@@ -53,26 +59,92 @@ int findString(FILE* fp, char* iString) {
 	//if elemRead is less than the acutal number of elements in the file, then we have a problem
 }
 
-int main(int argc, char* argv[]) {
+int sysFindStrings(int fp, char* iString) { //can do in large buffer (size of file using lseek)
+	int copy = 1; //flag that states if we found a copy
+	int copyCount = 0;
 
-	FILE* fp = fopen(argv[1], "r");
+	//finds size of file
+	lseek(fp, 0, SEEK_END); //set pointer to end of file
+	off_t size = lseek(fp, 0, SEEK_CUR); //get file size
+	assert(size < FILE_SIZE);
+	lseek(fp, 0, SEEK_SET); //set pointer back to begining of file
+	char* sysBuff = malloc(size);
+	int elemRead = read(fp, sysBuff, size);
+	sysBuff[elemRead] = 0; //assign terminating char where last char has been read
 
-	if(fp == NULL) {
-		perror("./p05");
-		exit(1);
-	}
+	// printf("File Contents:\n");
+	//printf("%s\n", fBuff);
+
+	//printf("%i\n", (int)strlen(iString));
 
 	int i = 0;
-	int countArray[argc - 2]; //each string must have an associated cout to it
-	i = 0;
-	for(;i < argc-2; ++i) {
-		countArray[i] = findString(fp, argv[i+2]); //remember, first arguement is executable file name, the second is the file we are reading
-		//printf("The count for string %s is %i.\n", argv[i+2], countArray[i]);
-		printf("%i\n", countArray[i]);
-		rewind(fp); //set pointer back to beginning of file
+	while(sysBuff[i] != '\0') {
+		if(tolower(sysBuff[i]) == tolower(iString[0])) {
+			int j = 1;
+			for(; j < strlen(iString); ++j) {
+				if(tolower(sysBuff[i + j]) != tolower(iString[j])) {
+					copy = 0;
+					break;
+				}
+			}
+			if(copy == 1) {
+				copyCount++;
+			}
+		}
+		i++;
+		copy = 1;
 	}
 
-	fclose(fp);
+	free(sysBuff);
+
+	return copyCount;
+}
+
+int main(int argc, char* argv[]) {
+
+	int sysFlag; //check if we are using system call
+	if (!strcmp(argv[2], "--systemcalls")) {
+		sysFlag = 1;
+	} else {
+		sysFlag = 0;
+	}
+	
+	if(sysFlag == 0) {
+
+		FILE* fp = fopen(argv[1], "r");
+
+		if(fp == NULL) {
+			perror("./p05");
+			exit(1);
+		}
+		int countArray[argc - 2]; //each string must have an associated cout to it
+		int i = 0;
+		for(;i < argc-2; ++i) {
+			countArray[i] = findString(fp, argv[i+2]); //remember, first arguement is executable file name, the second is the file we are reading
+			//printf("The count for string %s is %i.\n", argv[i+2], countArray[i]);
+			printf("%i\n", countArray[i]);
+			rewind(fp); //set pointer back to beginning of file
+		}
+		fclose(fp);
+	} else {
+
+		int fp = open(argv[1], O_RDONLY);
+
+		if(fp == -1) {
+			perror("./p05");
+			exit(1);
+		}
+
+		int countArray[argc - 3]; //each string must have an associated cout to it
+		int i = 0;
+		for(;i < argc-3; ++i) {
+			countArray[i] = sysFindStrings(fp, argv[i+3]); //remember, first arguement is executable file name, the second is the file we are reading, third is syscall flag
+			//printf("The count for string %s is %i.\n", argv[i+2], countArray[i]);
+			printf("%i\n", countArray[i]);
+			lseek(fp, 0, SEEK_SET); //set pointer back to beginning of file
+		}
+		close(fp);
+	}
 	//use an array of the size of each arguement which will save the matched chars
 	//if entire array matches the arguement, then we have a match within the file
 
